db002065fdcfba69fe759ea2386d15024507f4c7
jwangSD22 <jackw1689@gmail.com>
2023-05-06 21:05:13 -0700

    test display carousel for instant viewing

---
+++ src/component/MiniCarousel.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function MiniCarousel({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  const handlePrevClick = () => {
+    setCurrentIndex((currentIndex + images.length - 1) % images.length);
+  };
+
+  return (
+    <div>
+      <button onClick={handlePrevClick}>Prev</button>
+      <img src={images[currentIndex]} alt="Carousel" style={{ maxWidth: '600', maxHeight: '600px' }} />
+      <button onClick={handleNextClick}>Next</button>
+    </div>
+  );
+}
+
+export default MiniCarousel
--- src/modules/NewPost.js
+++ src/modules/NewPost.js
@@ -1,51 +1,147 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import MiniCarousel from "../component/MiniCarousel";
 
 function NewPost() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [body, setBody] = useState("");
+  const [images, setImages] = useState([]);
+  const [carouselURLs, setCarouselURLs] = useState([]);
+  const [finalPostKeys, setFinalPostKeys] = useState(null);
 
-    const [isLoggedIn,setIsLoggedIn] = useState(false)
-    const [user,setUser] = useState(null)
-    const navigate = useNavigate()
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        let response = await axios.get("/api/users/loginstatus");
+        console.log(response);
 
+        if (response.status === 200) {
+          setUser(response.data.user.jwtusername);
+          setIsLoggedIn(true);
+          return true;
+        }
+      } catch (err) {
+        console.log(err);
+        return navigate("/");
+      }
+    };
+    checkLogin();
+  }, []);
 
+  const handleBodyChange = (event) => {
+    setBody(event.target.value);
+  };
 
-    useEffect(()=>{
-        const checkLogin = async () => {
-         try{
-            let response = await axios.get('/api/users/loginstatus')
-            console.log(response)
-   
-            if(response.status===200){
-                console.log('logged in already')
-                setIsLoggedIn(true)
-                return true
-            }
-         }
-         catch(err){
-            console.log(err)
-            return navigate('/')
-         }
-  
+  const handleImageChange = async (event) => {
+    console.log(event.target.files);
+    const fileArray = event.target.files;
+    setImages(fileArray);
+    let tempArray = [];
+    for (let file of fileArray) {
+      let tempURL = URL.createObjectURL(file);
+      tempArray.push(tempURL);
+    }
 
-     }
-     checkLogin()
+    try {
+      await updateCarouselURLs(tempArray);
+    } catch (err) {
+      console.log(err);
+    }
 
-     },[])
+    console.log(carouselURLs);
+  };
 
+  const updateCarouselURLs = async (tempArray) => {
+    await new Promise((resolve) => {
+      setCarouselURLs(tempArray, resolve);
+    });
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
+    try {
+      await uploadPhotosToServer();
+    } catch (err) {
+      console.log(err);
+    }
+
+    try {
+      const response = await axios.post("/api/posts", {
+        author: user,
+        postMessage: body,
+        imageKeyArray: finalPostKeys,
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const uploadPhotosToServer = async () => {
+    const photoFormData = new FormData();
+
+    for (let i = 0; i < images.length; i++) {
+      photoFormData.append("files", images[i]);
+    }
+
+    try {
+      const response = await axios.post(
+        "/api/posts/imageupload",
+        photoFormData
+      );
+      setFinalPostKeys(response.data.finalObjectKeys);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    isLoggedIn?<div>POST PAGE</div>:<>not logged in</>
+    <div>
+      {isLoggedIn ? (
+        <div>
+          {" "}
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="body">Body:</label>
+              <textarea
+                id="body"
+                name="body"
+                rows="5"
+                cols="30"
+                value={body}
+                onChange={handleBodyChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="images">Images:</label>
+              <input
+                type="file"
+                id="images"
+                name="images"
+                multiple
+                onChange={handleImageChange}
+              />
+            </div>
 
-
-
-)
-
+            <button type="submit">Submit</button>
+          </form>
+          <button onClick={uploadPhotosToServer}>TEST UPLOAD</button>
+          <div>
+            {carouselURLs.length ? (
+              <MiniCarousel images={carouselURLs} />
+            ) : null}
+          </div>
+        </div>
+      ) : (
+        <>LOGGING IN....</>
+      )}
+    </div>
+  );
 }
 
-export default NewPost
+export default NewPost;
