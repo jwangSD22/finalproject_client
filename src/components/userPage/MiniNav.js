286e1f8839ab7e547a427b07116cdff537ccdd51
jwangSD22 <jackw1689@gmail.com>
2023-07-13 16:56:03 -0700

    mini-nav styling

--- src/components/userPage/MiniNav.js
+++ src/components/userPage/MiniNav.js
@@ -13,8 +13,8 @@ function MiniNav({viewFriendsToggle,setViewFriendsToggle}) {
   return (
     <div className="container mini-nav-box">
     <div className="row" style={{borderTop:'solid 1px #E4E6E9',paddingTop:'10px'}} >
-    <div className="mini-nav-link col-2 col-md-1 mx-2 bg-success" onClick={()=>{setViewFriendsToggle(false);window.location.reload()}}>Posts</div>
-    <div className="mini-nav-link col-2 col-md-1 mx-2 bg-danger" onClick={()=>{setViewFriendsToggle(true)}}>FRIENDS</div>
+    <div className={`mini-nav-link ${viewFriendsToggle?null:'nav-link-on'} col-2 col-md-1 mx-2 my-1`} onClick={()=>{setViewFriendsToggle(false);}}>Posts</div>
+    <div className={`mini-nav-link ${viewFriendsToggle?'nav-link-on':null} col-2 col-md-1 mx-2 my-1 ` }onClick={()=>{setViewFriendsToggle(true)}}>Friends</div>
     </div>
 
 
