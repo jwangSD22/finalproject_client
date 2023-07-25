242830e5861711c6e7678d1e799cb5b1b8bf19e0
jwangSD22 <jackw1689@gmail.com>
2023-07-24 22:03:32 -0700

    using new-password to bypass autocomplete

--- src/messenger/ConnectedChat.js
+++ src/messenger/ConnectedChat.js
@@ -146,7 +146,7 @@ function ConnectedChat({username1,username2,setChatConnected,chatConnected,userI
                 height: '40px',
               }}
               placeholder="Message"
-              autoComplete="off"
+              autoComplete="new-password"
             />
           </form>
 
