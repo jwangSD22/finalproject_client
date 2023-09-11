071485ad86f2b6900c3b563deb5d9e5e8a5decc5
jwangSD22 <jackw1689@gmail.com>
2023-09-11 11:25:03 -0700

    rmv stormdoor

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,60 +1,46 @@
-import React from 'react'
-import {Routes, Route, BrowserRouter,HashRouter} from 'react-router-dom'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import Friends from './pages/Friends'
-import User from './pages/User'
+import React from "react";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Friends from "./pages/Friends";
+import User from "./pages/User";
 
 //all of the below were used in backend testing//
-import App from './App'
-import NewPost from './modules/NewPost'
-import ChatRoom from './modules/ChatRoom'
-import Users from './modules/Users'
-import ErrorPage from './pages/ErrorPage'
-import Stormdoor from './modules/Stormdoor'
-
+import App from "./App";
+import NewPost from "./modules/NewPost";
+import ChatRoom from "./modules/ChatRoom";
+import Users from "./modules/Users";
+import ErrorPage from "./pages/ErrorPage";
+import Stormdoor from "./modules/Stormdoor";
 
 function RouteSwitch() {
-  
   return (
     <BrowserRouter>
-    <Routes>
+      <Routes>
+        {/**/}
+        <Route path="/" element={<Login />} />
 
-      {/**/}      
-      <Route path = "/" element={<Login />}/> 
+        {/*Home will display all friends' posts and have suggested contacts and contacts */}
+        <Route path="/home" element={<Home />} />
 
-      {/*Home will display all friends' posts and have suggested contacts and contacts */}
-      <Route path = "/home" element={<Home />}/> 
+        {/*Profile Route will display all of your personal posts */}
+        <Route path="/profile" />
 
-      {/*Profile Route will display all of your personal posts */}      
-      <Route path = "/profile" /> 
+        {/*Friend Route will display all friends and pending requests */}
+        <Route path="/friends" element={<Friends />} />
 
-      {/*Friend Route will display all friends and pending requests */}      
-      <Route path = "/friends" element={<Friends />} /> 
+        <Route path="/user/:username" element={<User />} />
 
-      <Route path = "/user/:username" element = {<User />}/>
-      
+        {/* use outlet from react router dom to handle nav between posts and friends? */}
 
-      
-      <Route path ="/stormdoor" element = {<Stormdoor />} />
-      {/* use outlet from react router dom to handle nav between posts and friends? */}
-
-        
-
-
-
-      <Route path ="/app" element={<App />} /> 
-      <Route path ="/createpost" element = {<NewPost />} />
-      <Route path='/chatroom/' element = {<ChatRoom />} />
-      <Route path='/users/' element = {<Users />} />
-      <Route path ='/error' element = {<ErrorPage />} />
-
-
-
-
-    </Routes>
+        <Route path="/app" element={<App />} />
+        <Route path="/createpost" element={<NewPost />} />
+        <Route path="/chatroom/" element={<ChatRoom />} />
+        <Route path="/users/" element={<Users />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default RouteSwitch
+export default RouteSwitch;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./navbar.css";
-import { HomeOutlined, MessageOutlined, TeamOutlined,MessageFilled } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  MessageOutlined,
+  TeamOutlined,
+  MessageFilled,
+} from "@ant-design/icons";
 import Searchbar from "./Searchbar";
 import RightNav from "./RightNav";
 
-const Navbar = ({ data, username,setMessengerOn,messengerOn}) => {
+const Navbar = ({ data, username, setMessengerOn, messengerOn }) => {
   const [path, setPath] = useState(null);
 
   //sets current path to alter css for center icons
@@ -15,92 +20,100 @@ const Navbar = ({ data, username,setMessengerOn,messengerOn}) => {
   return (
     <nav className="top-navbar container-fluid bg-white ">
       <div className="row">
-      {/* start-elements brand logo and search bar */}
-      <div className="left-nav col-6 col-md-4 d-flex justify-content-start align-items-center">
-        <div
-          className="navbar-brand me-2 mb-1 d-none d-sm-flex align-items-center"
->
-          <img
-            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-            height="20"
-            alt="MDB Logo"
-            loading="lazy"
-            style={{ marginTop: "2px" }}
-          />
-        </div>
-        {/* Search form */}
-      <Searchbar data={data} />
-    
-
-      </div>
-
-      {/* CENTER NAVIGATION */}
-      <div className="center-nav col-md-4 container d-none d-md-flex justify-content-center align-items-end">
-        {/* home */}
-        <a className={
-            path === "/home"
-              ? "current-path mx-1"
-              : "center-nav-container mx-1"
-          }
-          href="/home"
-          data-tooltip-id="right-tooltip"
-          data-tooltip-content="Home"
-        >
-          <div className="d-flex align-items-center">
-            <HomeOutlined className="top-icon"/>
+        {/* start-elements brand logo and search bar */}
+        <div className="left-nav col-6 col-md-4 d-flex justify-content-start align-items-center">
+          <div className="navbar-brand me-2 mb-1 d-none d-sm-flex align-items-center">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+              height="20"
+              alt="MDB Logo"
+              loading="lazy"
+              style={{ marginTop: "2px" }}
+            />
           </div>
-        </a>
+          {/* Search form */}
+          <Searchbar data={data} />
+        </div>
 
-        {/* friends */}
-
-        <a
-          className="d-flex align-items-center"
-          href="/friends"
-          data-tooltip-id="right-tooltip"
-          data-tooltip-content="Friends"
-        >
-          <div
+        {/* CENTER NAVIGATION */}
+        <div className="center-nav col-md-4 container d-none d-md-flex justify-content-center align-items-end">
+          {/* home */}
+          <a
             className={
-              path === "/friends"
+              path === "/home"
                 ? "current-path mx-1"
                 : "center-nav-container mx-1"
             }
+            href="/home"
+            data-tooltip-id="right-tooltip"
+            data-tooltip-content="Home"
           >
-           <TeamOutlined className="top-icon"/>
+            <div className="d-flex align-items-center">
+              <HomeOutlined className="top-icon" />
+            </div>
+          </a>
+
+          {/* friends */}
+
+          <a
+            className="d-flex align-items-center"
+            href="/friends"
+            data-tooltip-id="right-tooltip"
+            data-tooltip-content="Friends"
+          >
+            <div
+              className={
+                path === "/friends"
+                  ? "current-path mx-1"
+                  : "center-nav-container mx-1"
+              }
+            >
+              <TeamOutlined className="top-icon" />
+            </div>
+          </a>
+
+          {/* messenger */}
+          <div
+            className="d-flex align-items-center nav-messenger"
+            data-tooltip-id="right-tooltip"
+            data-tooltip-content="Messenger"
+            onClick={() => {
+              setMessengerOn(!messengerOn);
+            }}
+          >
+            <div className="center-nav-container mx-1">
+              {messengerOn ? (
+                <MessageFilled className="top-icon icon-filled mx-2" />
+              ) : (
+                <MessageOutlined className="messenger-icon mx-2" />
+              )}
+            </div>
           </div>
-        </a>
+        </div>
 
-        {/* messenger */}
-        <div
-          className="d-flex align-items-center nav-messenger"
-          data-tooltip-id="right-tooltip"
-          data-tooltip-content="Messenger"
-          onClick={()=>{setMessengerOn(!messengerOn)}}
-        >
-          <div className="center-nav-container mx-1">
-            {messengerOn?<MessageFilled className="top-icon icon-filled mx-2" />:<MessageOutlined className="messenger-icon mx-2" />}
+        {/* settings menu and profile page */}
+        <div className="end-nav col-6 col-md-4 d-flex justify-content-end">
+          {/* uses media queries here to determine screen size to generate the appropriate RightNav version with boolean prop */}
+          <div className="collapseTrue">
+            <RightNav
+              username={username}
+              data={data}
+              collapseState={true}
+              setMessengerOn={setMessengerOn}
+            />
+          </div>
 
+          <div className="collapseFalse">
+            <RightNav
+              username={username}
+              data={data}
+              collapseState={false}
+              setMessengerOn={setMessengerOn}
+            />
           </div>
         </div>
-        
-      </div>
 
-      {/* settings menu and profile page */}
-      <div className="end-nav col-6 col-md-4 d-flex justify-content-end">
-
-        {/* uses media queries here to determine screen size to generate the appropriate RightNav version with boolean prop */}
-        <div className="collapseTrue">
-          <RightNav username={username} data={data} collapseState={true} setMessengerOn={setMessengerOn}/>
-        </div>
-
-        <div className="collapseFalse">
-          <RightNav username={username} data={data} collapseState={false} setMessengerOn={setMessengerOn}/>
-        </div>
-      </div>
-
-      {/*tooltip styling - refer to rightnav where tooltip component settings are*/}
-      
-
+        {/*tooltip styling - refer to rightnav where tooltip component settings are*/}
       </div>
     </nav>
   );
